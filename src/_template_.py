8200d7ebc6d65d7101fdc7d9ff2dd64bbaad86d6
laws-spatial <andrewlawsmapping@gmail.com>
2023-12-02 08:35:29 -0600

    Created template

---
+++ src/_template_.py
@@ -0,0 +1,17 @@
+# imports
+from pathlib import Path
+
+# read data
+data_folder = Path("./data")
+day_number = 1
+day_text = "day" + str(day_number) + ".txt"
+data = data_folder / day_text
+
+# functions
+
+# main function
+def main():
+    fill_in = 0
+    
+if __name__ == "__main__":
+    main()
