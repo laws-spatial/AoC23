f8238b52ce6dc72cf8ebf48135dce94663124b74
laws-spatial <andrewlawsmapping@gmail.com>
2023-12-03 17:36:44 -0600

    Testing num_to_sides

--- src/day3a.py
+++ src/day3a.py
@@ -19,6 +19,12 @@ test_data = ["467..114..",
 "......755.",
 "...$.*....",
 ".664.598.."]
+
+# test = np.zeros((10, 10))
+# print(test)
+# test[5, 5] = "6"
+# print(test)
+
 # functions
 def queens_case_moves() -> list[np.array]:
     # set up all cases for checking values
@@ -35,7 +41,7 @@ def queens_case_moves() -> list[np.array]:
 
     return [UL, UU, UR, LL, RR, DL, DD, DR]
 
-def queens_case(data:list[str], position: np.array):
+def queens_case(data:list[str], position: np.array, tracking_array:np.array = None):
     for move in queens_case_moves():
         try:
             # apply move
@@ -47,16 +53,48 @@ def queens_case(data:list[str], position: np.array):
 
             # get data at that value
             data_value = data[data_pos_row][data_pos_col]
-        
-            print(f"Data value {data_value} at position {data_pos} with move {move}")
+
+            if data_value.isnumeric():
+                tracking_array[data_pos_row, data_pos_col] = int(data_value)
         except:
             continue
 
+def number_to_the_sides(tracking_array:np.array, data:list[str]=None):
+    for rowid in range(tracking_array.shape[0]):
+        for colid in range(tracking_array.shape[1]):
+            # get array value
+            arr_val = tracking_array[rowid, colid]
+
+            if arr_val > 0:
+                try:
+                    val_to_left = data[rowid][colid-1]
+                    if val_to_left.is_numeric():
+                        tracking_array[rowid, colid-1] = val_to_left
+                except:
+                    continue
+                try:
+                    val_to_right = data[rowid][colid+1]
+                    if val_to_right.is_numeric():
+                        tracking_array[rowid, colid+1] = val_to_right
+                except:
+                    continue
+
+    return number_to_the_sides()
+
+
+    
 # main function
 def main():
+
+    # tracking array
+    num_rows = len(test_data)
+    num_cols = len(test_data[0])
+
+    tracking_array = np.zeros((num_rows, num_cols))
+
+    # perform queens case on symbols
     for rowid, row in enumerate(test_data):
         for columnid, column in enumerate(row):
-
 
             # check if value is non-period symbol
             value = test_data[rowid][columnid]
@@ -65,7 +103,13 @@ def main():
                 # position
                 position = np.array([rowid, columnid])
 
-                queens_case(test_data, position)
+                queens_case(test_data, position, tracking_array)
+
+    # use tracking array to recursively look for numbers left and right of numbers
+    number_to_the_sides(tracking_array)
+
+
+    # print(tracking_array)
     
 if __name__ == "__main__":
     main()
